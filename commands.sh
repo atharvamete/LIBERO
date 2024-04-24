1eab6ed67ee7c559d938db8f9301ca328b0a6a3e
Atharva Anil Mete <amete7@login-phoenix-slurm-4.pace.gatech.edu>
2024-04-24 16:21:09 -0400

    pace evals

--- commands.sh
+++
@@ -1,1 +0,0 @@
-sbatch slurm/run.sbatch python libero/lifelong/skill_policy_eval.py pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_finetune_clip/LIBERO_10/Multitask/SkillGPT_Model/ResnetEncoder/m4_32_f4_k3s4_tt_5shot/run_001/multitask_model_ep50.pth" exp_name="eval_m4_32_f4_k3s4_tt_5shot_e50" 
