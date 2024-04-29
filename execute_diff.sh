5c9a7acc31b19caf19e88ad18913ec4d751c97ce
Atharva Anil Mete <amete7@login-phoenix-slurm-3.pace.gatech.edu>
2024-04-29 15:28:16 -0400

    fewshot evals done

--- execute_diff.sh
+++ execute_diff.sh
@@ -1,18 +1,6 @@
-sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
-    pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_clip/LIBERO_90/Multitask/VQBet_Model/ResnetEncoder/vqbet_a1_l1h128_c32_n2_n6d120/run_001/multitask_model_ep80.pth" \
-    exp_name="eval40_lib90_vqbet_a1_l1h128_c32_n2_n6d120_ep80" \
-    data.seq_len=10 \
-    policy.skill_block_size=1 \
-    policy.hidden_dim=128 \
-    policy.num_layers=1 \
-    policy.codebook_size=32 \
-    policy.gpt_n_embd=120 \
-    policy.mpc_horizon=1 \
-    benchmark_name="LIBERO_90" 
-
-sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
+sbatch slurm/few.sbatch python libero/lifelong/few_shot.py \
     pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_clip/LIBERO_90/Multitask/VQBet_Model/ResnetEncoder/vqbet_a5_l1h128_c32_n2_n6d120/run_001/multitask_model_ep60.pth" \
-    exp_name="eval40_lib90_vqbet_a5_l1h128_c32_n2_n6d120_ep60" \
+    exp_name="vqbet_a5_l1h128_c32_n2_n6d120_5shot" \
     data.seq_len=14 \
     policy.skill_block_size=5 \
     policy.hidden_dim=128 \
@@ -20,5 +8,4 @@ sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
     policy.codebook_size=32 \
     policy.gpt_n_embd=120 \
     policy.mpc_horizon=5 \
-    benchmark_name="LIBERO_90" 
-
+    benchmark_name="LIBERO_10" 
---
+++ execute_few_vqbet.sh
@@ -0,0 +1,59 @@
+sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
+    pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_finetune_clip/LIBERO_10/Multitask/VQBet_Model/ResnetEncoder/vqbet_a5_l1h128_c32_n2_n6d120_5shot/run_001/multitask_model_ep100.pth" \
+    exp_name="eval40_vqbet_a5_l1h128_c32_n2_n6d120_5shot_ep100" \
+    data.seq_len=14 \
+    policy.skill_block_size=5 \
+    policy.hidden_dim=128 \
+    policy.num_layers=1 \
+    policy.codebook_size=32 \
+    policy.gpt_n_embd=120 \
+    policy.mpc_horizon=5 \
+    benchmark_name="LIBERO_10" 
+
+sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
+    pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_finetune_clip/LIBERO_10/Multitask/VQBet_Model/ResnetEncoder/vqbet_a5_l1h128_c32_n2_n6d120_5shot/run_001/multitask_model_ep80.pth" \
+    exp_name="eval40_vqbet_a5_l1h128_c32_n2_n6d120_5shot_ep80" \
+    data.seq_len=14 \
+    policy.skill_block_size=5 \
+    policy.hidden_dim=128 \
+    policy.num_layers=1 \
+    policy.codebook_size=32 \
+    policy.gpt_n_embd=120 \
+    policy.mpc_horizon=5 \
+    benchmark_name="LIBERO_10" 
+
+sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
+    pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_finetune_clip/LIBERO_10/Multitask/VQBet_Model/ResnetEncoder/vqbet_a5_l1h128_c32_n2_n6d120_5shot/run_001/multitask_model_ep50.pth" \
+    exp_name="eval40_vqbet_a5_l1h128_c32_n2_n6d120_5shot_ep50" \
+    data.seq_len=14 \
+    policy.skill_block_size=5 \
+    policy.hidden_dim=128 \
+    policy.num_layers=1 \
+    policy.codebook_size=32 \
+    policy.gpt_n_embd=120 \
+    policy.mpc_horizon=5 \
+    benchmark_name="LIBERO_10" 
+
+sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
+    pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_finetune_clip/LIBERO_10/Multitask/VQBet_Model/ResnetEncoder/vqbet_a5_l1h128_c32_n2_n6d120_5shot/run_001/multitask_model_ep20.pth" \
+    exp_name="eval40_vqbet_a5_l1h128_c32_n2_n6d120_5shot_ep20" \
+    data.seq_len=14 \
+    policy.skill_block_size=5 \
+    policy.hidden_dim=128 \
+    policy.num_layers=1 \
+    policy.codebook_size=32 \
+    policy.gpt_n_embd=120 \
+    policy.mpc_horizon=5 \
+    benchmark_name="LIBERO_10" 
+
+sbatch slurm/eval.sbatch python libero/lifelong/skill_policy_eval.py \
+    pretrain_model_path="/storage/home/hcoda1/0/amete7/p-agarg35-0/diff-skill/LIBERO/experiments_finetune_clip/LIBERO_10/Multitask/VQBet_Model/ResnetEncoder/vqbet_a5_l1h128_c32_n2_n6d120_5shot/run_001/multitask_model_ep10.pth" \
+    exp_name="eval40_vqbet_a5_l1h128_c32_n2_n6d120_5shot_ep10" \
+    data.seq_len=14 \
+    policy.skill_block_size=5 \
+    policy.hidden_dim=128 \
+    policy.num_layers=1 \
+    policy.codebook_size=32 \
+    policy.gpt_n_embd=120 \
+    policy.mpc_horizon=5 \
+    benchmark_name="LIBERO_10" 
